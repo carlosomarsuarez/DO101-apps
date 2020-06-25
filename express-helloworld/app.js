f22991739d421f3b7cfc02dc7785fe8dfec5f3bb
Carlos Omar Suarez <carlosomarsuarez@gmail.com>
2020-06-25 17:54:16 -0500

    Version 1.1

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,8 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-  res.send('This is version 1.\n');
+  res.send('Hello World!\nThis is version 1.\n');
 });
 
 app.get('/mars',function(req,res){
